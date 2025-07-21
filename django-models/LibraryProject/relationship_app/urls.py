012c3ff50637d498d261569c83a399fde36ef752
Karinge Muniu <karingemuniu@gmail.com>
2025-07-21 11:53:14 +0300

    Added authentication URLs for checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import user_login, user_logout, register
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -10,4 +13,9 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('register/', register, name='register'),
+]
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
