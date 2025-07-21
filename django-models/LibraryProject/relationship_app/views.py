cbcacc5850ac77ddc2fadb7075ba4c5a02a33d7c
Karinge Muniu <karingemuniu@gmail.com>
2025-07-21 13:15:58 +0300

    Added custom permissions and updated book URLs in relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -114,19 +114,25 @@ def delete_book(request, pk):
         return redirect('list_books')
     return render(request, 'relationship_app/delete_book.html', {'book': book})
 
+
+
 from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
 
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_book_view(request):
-    
-    pass
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    # Your book creation logic here
+    return render(request, 'relationship_app/add_book.html')
 
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book_view(request):
-    
-    pass
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Your book editing logic here
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
 
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book_view(request):
-    
-    pass
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.delete()
+    return redirect('book_list')  # Replace with your actual book list view name
