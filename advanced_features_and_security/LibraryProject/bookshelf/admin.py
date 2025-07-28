3d85b09455a331caa22cbd37f6bb5a9cc1e29cf6
Karinge Muniu <karingemuniu@gmail.com>
2025-07-28 14:47:42 +0300

    Fix: Register CustomUser in bookshelf/admin.py for checker

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,16 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
 
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
