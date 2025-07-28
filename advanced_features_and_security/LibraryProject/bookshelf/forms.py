28e0282b2788a18c8746a8e3ec2354bf3fedbb6b
Karinge Muniu <karingemuniu@gmail.com>
2025-07-28 21:46:13 +0300

    Add ExampleForm to forms.py as required by ALX checker

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,10 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+
+
+from django import forms
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea)
