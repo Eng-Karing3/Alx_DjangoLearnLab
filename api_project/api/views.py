8915915136e05328933aec5b248ce7be318edf85
Karinge Muniu <karingemuniu@gmail.com>
2025-08-03 19:03:54 +0300

    Added the required extention

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,7 +3,7 @@ from .models import Book
 from rest_framework import generics
 from .serializers import BookSerializer
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
 
