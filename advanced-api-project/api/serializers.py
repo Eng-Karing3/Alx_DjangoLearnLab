04bbd41ca86fbafb616482096a40c2c5346e576c
Karinge Muniu <karingemuniu@gmail.com>
2025-08-08 20:53:58 +0300

    fixed the error

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,7 +2,15 @@ from .models import Author, Book
 from rest_framework import serializers
 
 
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'publication_date']
+
+
 class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
     class Meta:
         model = Author
         fields = ['id', 'name']
