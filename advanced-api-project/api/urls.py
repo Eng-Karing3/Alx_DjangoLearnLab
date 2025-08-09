4df70a85157cada1b1a3bbb335fdb62ec47347e8
Karinge Muniu <karingemuniu@gmail.com>
2025-08-09 20:52:08 +0300

    Updated the URLs

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import BookListCreateView, BookRetrieveUpdateDestroyView
 from rest_framework.routers import DefaultRouter
+from . import views
 
 urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
+    path('books/', views.ListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
+    path('books/create/', views.CreateView.as_view(), name='book-create'),
+    path('books/update/<int:pk>/', views.UpdateView.as_view(), name='book-update'),
+    path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='book-delete'),
 ]
