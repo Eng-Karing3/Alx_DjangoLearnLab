ad916e515e355ead4cf2e065c3c5efcb40012228
Karinge Muniu <karingemuniu@gmail.com>
2025-08-17 21:02:25 +0300

    Updated models.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -118,13 +118,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static',
 ]
 
-TEMPLATES = [
-    {
-        'DIRS': [BASE_DIR / 'templates'],
-    },
+TEMPLATES_DIRS = [
+    BASE_DIR / 'templates',
 ]
 
 # Default primary key field type
