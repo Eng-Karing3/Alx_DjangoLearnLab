f5d1a51a0566bdb1933eea7df22091517fe5998b
Karinge Muniu <karingemuniu@gmail.com>
2025-08-17 23:33:14 +0300

    Implemented full CRUD for blog posts with authentication and permissions

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+
 
 from .forms import (
     CustomUserCreationForm,
@@ -48,5 +53,56 @@ def profile(request):
         "p_form": p_form,
     }
     return render(request, "blog/profile.html", context)
+
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'   # template for listing
+    context_object_name = 'posts'
+    ordering = ['-created_at']  # newest first
+
+
+# Show details of one post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+# Create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # set author automatically
+        return super().form_valid(form)
+
+
+# Update/Edit a post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+# Delete a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'   # after deleting, go back home
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
 
 # Create your views here.
